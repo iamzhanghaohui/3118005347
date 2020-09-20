4a5799c992e372b8af009d01556af73f5cb467ab
iamzhanghaohui <49125335+iamzhanghaohui@users.noreply.github.com>
2020-09-20 11:46:45 +0800

    （初始化）初始化仓库

---
+++ src/main/java/com/free/main.java
@@ -0,0 +1,14 @@
+package com.free;
+
+/**
+ * @ClassNamemain
+ * @Description
+ * @Author Free
+ * @Date2020/9/20 11:40
+ * @Version V1.0
+ **/
+public class main {
+    public static void main(String[] args) {
+        System.out.println("初始化仓库");
+    }
+}
