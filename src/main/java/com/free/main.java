f808047188e0f98d761836506a4a5361817f8325
iamzhanghaohui <49125335+iamzhanghaohui@users.noreply.github.com>
2020-09-20 12:30:15 +0800

    （增加）引入糊涂工具包

--- src/main/java/com/free/main.java
+++ src/main/java/com/free/main.java
@@ -1,4 +1,5 @@
 package com.free;
+import cn.hutool.core.io.file.FileReader;
 
 /**
  * @ClassNamemain
@@ -10,5 +11,10 @@ package com.free;
 public class main {
     public static void main(String[] args) {
         System.out.println("初始化仓库");
+        //默认UTF-8编码，可以在构造中传入第二个参数做为编码
+        FileReader fileReader = new FileReader("D:\\Free\\课程\\软件工程\\数据源\\test\\test\\orig.txt");
+        String result = fileReader.readString();
+        System.out.println(result);
+
     }
 }
